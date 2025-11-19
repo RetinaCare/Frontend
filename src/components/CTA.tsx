f8fa18ba6439326f887da5c98334568b054a4422
Belovethdev <belovetholuwagbemiga2002@gmail.com>
2025-11-19 14:44:03 +0100

    homepage updated

---
+++ src/components/CTA.tsx
@@ -0,0 +1,72 @@
+import React, { useRef } from "react";
+import { motion, useInView, useScroll, useTransform } from "framer-motion";
+
+const backgroundImage = "/public/images/doct.jpg"; 
+
+
+const CTAHealthBanner: React.FC = () => {
+  const sectionRef = useRef(null);
+  const isInView = useInView(sectionRef, { once: true, margin: "-120px" });
+
+  // Enhanced parallax
+  const { scrollY } = useScroll();
+  const parallaxY = useTransform(scrollY, [0, 800], [0, 120]);
+
+  return (
+    <section ref={sectionRef} className="w-full md:px-26 py-8n flex justify-center px-4">
+      <div className="relative w-full max-w-[1432px] rounded-4xl overflow-hidden shadow-md">
+
+        {/* BACKGROUND IMAGE */}
+        <motion.img
+          src={backgroundImage}
+          alt="Why choose us"
+          className="w-full h-[500px] md:h-[900px] object-cover"
+          style={{ y: parallaxY }}
+          initial={{ scale: 1.18, opacity: 0 }}
+          animate={isInView ? { scale: 1, opacity: 1 } : {}}
+          transition={{
+            duration: 2.5,
+            ease: [0.16, 1, 0.3, 1], 
+          }}
+        />
+
+        {/* LEFT BUBBLE */}
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          viewport={{ once: true }}
+          className="
+            absolute top-10 left-10
+            bg-[#63E6BE]
+            px-6 py-4 rounded-[20px]
+            shadow-md max-w-[260px]
+            text-[#1B3A3A] text-[22px] font-semibold leading-snug
+          "
+        >
+          Take the first step to <br /> better health
+        </motion.div>
+
+        {/* RIGHT BUBBLE */}
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 0.15 }}
+          viewport={{ once: true }}
+          className="
+            absolute bottom-10 right-10
+            bg-[#63E6BE]
+            px-6 py-4 rounded-[20px]
+            shadow-md max-w-[260px]
+            text-[#1B3A3A] text-[22px] font-semibold leading-snug
+          "
+        >
+          Schedule your <br /> appointments today
+        </motion.div>
+
+      </div>
+    </section>
+  );
+};
+
+export default CTAHealthBanner;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,28 @@
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Mission from "../components/Mission";
+import ServiceHighlights from "../components/Service";
+import WhyChooseUs from "../components/WhyYouChoose";
+import Testimonials from "../components/Testimonials";
+// import MeetSpecialists from "../components/Specialists";
+// import CTAHealthBanner from "../components/CTA";
+import PartnersStrip from "../components/Partner";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-     <div className="min-h-screen bg-white">
-      {/* Main container with exact Figma dimensions */}
-      <div 
-        className="mx-auto overflow-hidden h-[4496px] rounded-[20px_20px_20px_20px] bg-[#90D5FF99]"
-        style={{
-          // width: '1440px',
-          maxWidth: '100%', // For responsiveness
-          // borderRadius: '20px',
-          // background: 'rgba(144, 213, 255, 0.6)' // #90D5FF99 - 60% opacity
-        }}
-      >
+     <div className="w-full bg-[#FFFFFF]  min-h-screen">
+      <div>
       <Navbar />
-
-      {/* Page Content */}
-    
-
-        {/* Your landing page sections will go here */}
+      <Hero />
+      <Mission />
+      <ServiceHighlights />
+      <WhyChooseUs />
+      <Testimonials /> 
+      {/* <MeetSpecialists /> */}
+      {/* <CTAHealthBanner /> */}
+      <PartnersStrip />
+      <Footer />
       </div>
     </div>
   );
